4b162738bede3ec45f8133bf12a2b15e4edd2ae0
ZafarUrakov <goliburakov535@gmail.com>
2024-01-27 08:12:46 +0300

    ShouldThrowPreconditionFailedException -> FAIL

---
+++ STX.EFxceptions.Cosmos.Base/Models/Exceptions/PreconditionFailedException.cs
@@ -0,0 +1,13 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+
+namespace STX.EFxceptions.Cosmos.Base.Models.Exceptions
+{
+    public class PreconditionFailedException : Exception
+    {
+        public PreconditionFailedException(string message) : base(message) { }
+    }
+}
