176823d02990c911dbeb2ddfa48cbb2bec9aa8f3
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-27 19:17:23 +0900

    ShouldThrowInternalServerCosmosException -> FAIL

--- STX.EFxceptions.Cosmos.Base.Tests.Unit/Services/Foundations/CosmosEfxceptionServiceTests.Exceptions.cs
+++ STX.EFxceptions.Cosmos.Base.Tests.Unit/Services/Foundations/CosmosEfxceptionServiceTests.Exceptions.cs
@@ -170,5 +170,21 @@ namespace STX.EFxceptions.Cosmos.Base.Tests.Unit.Services.Foundations
             Assert.Throws<TooManyRequestsCosmosException>(() =>
                 this.cosmosEFxceptionService.ThrowMeaningfulException(cosmosException));
         }
+
+        [Fact]
+        public void ShouldThrowInternalServerCosmosException()
+        {
+            // given
+            HttpStatusCode cosmosStatusCode = (HttpStatusCode)449;
+            CosmosException cosmosException = CreateCosmosException(cosmosStatusCode);
+
+            this.cosmosErrorBrokerMock.Setup(broker =>
+                broker.GetErrorCode(cosmosException))
+                    .Returns((int)cosmosStatusCode);
+
+            // when . then
+            Assert.Throws<InternalServerCosmosException>(() =>
+                this.cosmosEFxceptionService.ThrowMeaningfulException(cosmosException));
+        }
     }
 }
