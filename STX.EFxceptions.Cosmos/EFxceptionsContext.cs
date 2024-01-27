c9b88e50e87e0a28c1d65d48e1c06ea3412eb7de
ZafarUrakov <goliburakov535@gmail.com>
2024-01-27 13:33:44 +0300

    EXPOSERS: Initialize EFxceptionsContext

---
+++ STX.EFxceptions.Cosmos/EFxceptionsContext.cs
@@ -0,0 +1,22 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Cosmos;
+using STX.EFxceptions.Core;
+using STX.EFxceptions.Cosmos.Base.Brokers.DbErrorBroker;
+using STX.EFxceptions.Cosmos.Base.Services.Foundations;
+using STX.EFxceptions.Interfaces.Brokers.DbErrorBroker;
+using STX.EFxceptions.Interfaces.Services.EFxceptions;
+
+namespace STX.EFxceptions.Cosmos
+{
+    public class EFxceptionsContext : DbContextBase<CosmosException>
+    {
+        protected override IDbErrorBroker<CosmosException> CreateErrorBroker() =>
+            new CosmosErrorBroker();
+
+        protected override IEFxceptionService<CosmosException> CreateEFxceptionService(
+            IDbErrorBroker<CosmosException> errorBroker) => new CosmosEFxceptionService(errorBroker);
+    }
+}
