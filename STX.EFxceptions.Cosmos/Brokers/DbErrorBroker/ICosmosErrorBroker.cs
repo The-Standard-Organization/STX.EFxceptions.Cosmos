edfe0f9ac454678387f8397c9811898c81647fb3
ZafarUrakov <goliburakov535@gmail.com>
2024-01-27 06:11:42 +0300

    BROKERS: Get Error Code

---
+++ STX.EFxceptions.Cosmos/Brokers/DbErrorBroker/ICosmosErrorBroker.cs
@@ -0,0 +1,12 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Cosmos;
+using STX.EFxceptions.Interfaces.Brokers.DbErrorBroker;
+
+namespace STX.EFxceptions.Cosmos.Brokers.DbErrorBroker
+{
+    public interface ICosmosErrorBroker : IDbErrorBroker<CosmosException>
+    { }
+}
