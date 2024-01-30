2d4de26ce4ff9aa0f09265a94688520f270daeb2
ZafarUrakov <goliburakov535@gmail.com>
2024-01-30 14:46:12 +0300

    ShouldSaveChangesSuccessfully -> PASS

---
+++ STX.EFxceptions.Cosmos.Tests.Unit/EFxceptionsContextTests.Logic.cs
@@ -0,0 +1,32 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using STX.EFxceptions.Cosmos.Base.Models.Exceptions;
+using STX.EFxceptions.Cosmos.Tests.Acceptance.Models;
+using Xunit;
+
+namespace STX.EFxceptions.Cosmos.Tests.Acceptance
+{
+    public partial class EFxceptionsContextTests
+    {
+        [Fact]
+        public void ShouldSaveChangesSuccessfully()
+        {
+            // given
+            Client client = new Client
+            {
+                Id = new System.Guid("e02a866b-1266-4033-93a2-ea94ac457ee8"),
+            };
+
+            // when
+            context.Clients.Add(client);
+            context.SaveChanges();
+
+            // then
+            context.Clients.Remove(client);
+            context.SaveChanges();
+        }
+    }
+}
