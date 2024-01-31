b83cc38aada55402f225a13dd6b39f6cb38f2077
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-31 23:14:49 +0900

    ShouldSaveChangesSuccessfully -> PASS

---
+++ STX.EFxceptions.Identity.Cosmos.Tests.Acceptance/EFxceptionsIdentityContextTests.Logic.cs
@@ -0,0 +1,31 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using STX.EFxceptions.Identity.Cosmos.Tests.Acceptance.Models.Clients;
+using Xunit;
+
+namespace STX.EFxceptions.Identity.Cosmos.Tests.Acceptance
+{
+    public partial class EFxceptionsIdentityContextTests
+    {
+        [Fact]
+        public void ShouldSaveChangesSuccessfully()
+        {
+            // given
+            var client = new Client
+            {
+                Id = new Guid("e02a866b-1266-4033-93a2-ea94ac457ee8")
+            };
+
+            // when
+            context.Clients.Add(client);
+            context.SaveChanges();
+
+            // then
+            context.Clients.Remove(client);
+            context.SaveChanges();
+        }
+    }
+}
