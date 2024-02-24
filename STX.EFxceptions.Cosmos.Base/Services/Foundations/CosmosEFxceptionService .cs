4d0269fede7404d2c00a8987bdd3cecaeb70c3e5
Zafar Urakov <138094771+ZafarUrakov@users.noreply.github.com>
2024-02-24 23:43:31 +0500

    CODE RUB: Delete Error Code

--- STX.EFxceptions.Cosmos.Base/Services/Foundations/CosmosEFxceptionService .cs
+++ STX.EFxceptions.Cosmos.Base/Services/Foundations/CosmosEFxceptionService .cs
@@ -16,15 +16,14 @@ namespace STX.EFxceptions.Cosmos.Base.Services.Foundations
         public CosmosEFxceptionService(IDbErrorBroker<CosmosException> cosmosErrorBroker) =>
             this.cosmosErrorBroker = cosmosErrorBroker;
 
-        public void ThrowMeaningfulException(DbUpdateException dbUpdateException) =>
-        TryCatch(() =>
+        public void ThrowMeaningfulException(DbUpdateException dbUpdateException)
         {
             ValidateInnerException(dbUpdateException);
             CosmosException cosmosException = GetCosmosException(dbUpdateException.InnerException);
             int cosmosErrorCode = this.cosmosErrorBroker.GetErrorCode(cosmosException);
             ConvertAndThrowMeaningfulException(cosmosErrorCode, cosmosException.Message);
             throw dbUpdateException;
-        });
+        }
 
         private CosmosException GetCosmosException(Exception exception) => (CosmosException)exception;
     }
